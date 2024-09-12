95f53b20fc9bb41278816f69a53e60f69d52327c
Durga-Sirisha-Karuturi <84554281+Durga-Sirisha-Karuturi@users.noreply.github.com>
2024-09-12 10:12:00 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
-# tests/test_app.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import hello_world
 
 def test_hello_world():
